7c06f420b6d5c813fe6ea34994c6c78a5f1432a4
Fernando Kon <fernando.freire@lab2dev.com>
2024-01-11 14:34:35 +0000

    feat tables + searchField

---
+++ webapp/controller/SessionsDetail.controller.js
@@ -0,0 +1,41 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+],
+    /**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+    function (Controller, JSONModel, Filter, FilterOperator) {
+        "use strict";
+
+        return Controller.extend("com.lab2dev.btpexperience.controller.SessionsDetail", {
+            onInit: function () {
+                const oModel = new JSONModel();
+                oModel.loadData("/model/participantes.json");
+
+                this.getView().setModel(oModel, "participantes");
+            },
+
+            onSearch: function (oEvent) {
+                const sQuery = oEvent.getSource().getValue();
+
+                const aFilters = [];
+
+                if (sQuery && sQuery.length > 0) {
+                    const filterOrders = new Filter({
+                        filters: [
+                            new Filter("Name", FilterOperator.Contains, sQuery), 
+                            new Filter("Enterprise", FilterOperator.EQ, sQuery)
+                        ]
+                    });
+                    aFilters.push(filterOrders);
+                }
+
+                const oGridList = this.byId("idTableParticipant");
+                const oBinding = oGridList.getBinding("rows");
+                oBinding.filter(aFilters);
+            },
+        });
+    });
